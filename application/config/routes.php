7d4694fc7ac8db0eb48373f01a48cdba3b82321f
caseychoiniere <caseychoiniere@gmail.com>
2015-04-20 14:41:23 -0700

    Adds create view and model for insertion of item into DB

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,6 +54,7 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 */
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
