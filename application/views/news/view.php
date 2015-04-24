846027fb9aee4206237d60c501a30d255369d22f
caseychoiniere <caseychoiniere@gmail.com>
2015-04-23 17:54:34 -0700

    Adds dynamic template and theme

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,12 +1,3 @@
 <?php
-//$this->load->view('themes/bootswatch/header');
-$this->load->view($this->config->item('theme') . 'header');
-?>
-
-<?php
 echo '<h2>'.$news_item['title'].'</h2>';
 echo $news_item['text'];
-
-//$this->load->view('themes/bootswatch/footer');
-$this->load->view($this->config->item('theme') . 'footer');
-?>
